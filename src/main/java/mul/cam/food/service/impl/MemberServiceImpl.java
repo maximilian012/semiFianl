f12a5c5d8449f5f6c50307d8b34bd761e66353ad
jyeonshin <94603624+jyeonshin@users.noreply.github.com>
2023-03-14 17:54:15 +0900

    add bbslist page

--- src/main/java/mul/cam/food/service/impl/MemberServiceImpl.java
+++ src/main/java/mul/cam/food/service/impl/MemberServiceImpl.java
@@ -1,12 +1,42 @@
 package mul.cam.food.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import mul.cam.food.dao.MemberDao;
+import mul.cam.food.dto.MemberDto;
 import mul.cam.food.service.MemberService;
 
-
-@Service // dao와 controller사이에 교두보 역할 
+@Service
 public class MemberServiceImpl implements MemberService{
+	
+	@Autowired
+	MemberDao dao;
+	
+	@Override
+	public List<MemberDto> allMember() {
+		return dao.allMember();
 
+	}
+	
+	@Override
+	public boolean idCheck(String id) {
+		int count = dao.idCheck(id);
+		return count>0?true:false;
+	}
+	
+	@Override
+	public boolean addMember(MemberDto dto) {
+		int count = dao.addMember(dto);
+		return count>0?true:false;
+	}
+	
+	@Override
+	public MemberDto login(MemberDto dto) {
+		return dao.login(dto);
+	}
+	
 	
 }
